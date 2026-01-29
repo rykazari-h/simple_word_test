dec073f0b517209218737f8754b84348e490f346
rykazari <ryota.books@outlook.jp>
2026-01-29 23:27:03 +0900

    tuik

--- script.js
+++ script.js
@@ -18,7 +18,9 @@ const examples = [
 	"kikutan_week3.txt",
 	"kikutan_week4.txt",
 	"kikutan_week5.txt",
-	"kikutan_week6.txt"
+	"kikutan_week6.txt",
+	"kikutan_week7.txt",
+	"kikutan_week8.txt"
 ];
 const fileCells = document.getElementById("fileCells");
 const addCellBtn = document.getElementById("addCell");
